d766e218029dd4ec5e903bdd9aee256a28c51de1
Daniel Yang <145522249+daniel-yang66@users.noreply.github.com>
2026-04-27 22:17:36 -0400

    airport diagram api update

--- app/api/airportDiagram.js
+++ app/api/airportDiagram.js
@@ -1,10 +1,15 @@
 export default async function AirportDiagram(airport) {
-  let data;
-  const res = await fetch(
-    `https://api.aviationapi.com/v1/charts?apt=${airport}&group=2`
-  );
-  data = await res.json();
-  data = data[`${airport}`][0] ? data[`${airport}`][0]["pdf_path"] : null;
-
-  return data;
+  try {
+    let data;
+    const res = await fetch(
+      `https://api-v2.aviationapi.com/v2/charts?airport=${airport}&airac=0`
+    );
+    data = await res.json();
+    data = data.charts && data.charts.airport_diagram  ? data.charts.airport_diagram[0].pdf_url : null;
+  
+    return data
+  }
+  catch {
+    return null
+}
 }
